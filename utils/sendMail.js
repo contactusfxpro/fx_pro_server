ef653b0bd85ff503d5a5eb1e8347aa88cb1cd50f
Abishek6702 <abishekkrishnat@gmail.com>
2026-01-06 13:55:14 +0530

    6-1-20263

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -7,9 +7,6 @@ const sendMail = async (to, subject, html) => {
       user: process.env.EMAIL_USER, // Email address
       pass: process.env.EMAIL_PASS, // Email password or app password
     },
-   connectionTimeout: 5000,
-  greetingTimeout: 5000,
-  socketTimeout: 5000,
   });
 
   const mailOptions = {
