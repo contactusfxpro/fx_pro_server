6991aec96c59ca22a374a8396ecbd2ea0f24ac66
Abishek6702 <abishekkrishnat@gmail.com>
2026-01-06 12:42:36 +0530

    6-1-2026

--- middleware/ratelimit.js
+++ middleware/ratelimit.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const fxLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 30,
+  max: 120,
   message: {
     error: "Too many requests. Please slow down.",
   },
